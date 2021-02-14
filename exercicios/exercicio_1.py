88ce3fa78961f89400bd9cf724c892fa78b28033
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-14 15:29:46 -0300

    reafatorando o estudo

---
+++ exercicios/exercicio_1.py
@@ -0,0 +1,11 @@
+nome = 'Marcelo'
+idade = 29
+altura = 1.75
+e_maior = idade > 18
+peso = 102.9
+ano_nascimento = 2020 - idade
+imc = peso / altura ** 2
+
+print(f'{nome} tem {idade} anos, {altura} de altura e pesa {peso}')
+print(f'O IMC de {nome} é {imc}')
+print(f'{nome} nasceu em {ano_nascimento}')
