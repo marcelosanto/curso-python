5e08bebd44ce31fc1f72a5093b64af881bf2481a
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-14 15:02:38 -0300

    tipos de dados

---
+++ tipos_de_dados.py
@@ -0,0 +1,27 @@
+# Tipos de Dados
+"""
+str - string - textos
+int - inteiro - 123456789 -> 99999999
+float - real/ponto flutuante - 10.50 1.5 - 10.99
+bool - booleano/logico - True/False
+"""
+
+print('Marcelo', type('Marcelo'))
+print(True, type(True))
+print(10, type(10))
+print(10.1, type(10.1))
+
+# type casting
+print('Marcelo', type('Marcelo'), bool('Marcelo'))
+
+# String: nome
+print('Marcelo', type('Marcelo'))
+
+# Idade: int
+print(29, type(29))
+
+# Altura: float
+print(1.75, type(1.75))
+
+# É maior de idade 10 > 20
+print(29 > 18, type(29 > 18))
