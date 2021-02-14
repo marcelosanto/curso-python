f4c12840b884a5be34b5ae7ce7d8013d5fccaa82
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-14 15:08:57 -0300

    operadores aritimeticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,12 @@
+"""
++, -, *, /, //, **, %, ()
+"""
+
+print('Multiplicação *', 10*10)
+print('Soma +', 10+10)
+print('Subtração -', 10-10)
+print('Divisão /', 10/10)
+print('Divisão inteira //', 10.2/3)
+
+print('Resto %', 84 % 2 == 1)
+print('potenciação **', 10**2)
