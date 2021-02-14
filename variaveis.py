e6129d7c529666d585067252f37474a02e177d5a
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-14 15:15:33 -0300

    variaveis

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+# Variaveis
+# inicia com letra, numeros, separar _, letras minusculas
+
+nome = 'Marcelo'
+idade = 29
+altura = 1.75
+e_maior = idade > 18
+peso = 102.9
+
+imc = peso / (altura * altura)
+
+print(nome, 'tem', idade, 'de idade e seu imc é', imc)
