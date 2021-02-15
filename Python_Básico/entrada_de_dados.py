2ac04825000cfce360c5d82893556de605f3a070
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-15 05:19:35 -0300

    continua

--- Python_Básico/entrada_de_dados.py
+++ Python_Básico/entrada_de_dados.py
@@ -1,1 +1,9 @@
 # Entrada de dados
+
+nome = input('Qual o seu nome? ')
+idade = input('Qual e a sua idade? ')
+ano_nascimento = 2020 - int(idade)
+
+print()
+print(f'O {nome} tem {idade} anos. '
+      f'{nome} nasceu em {ano_nascimento}')
