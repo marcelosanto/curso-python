e92d4b942a589551afe558800438a90528b61ccc
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-15 11:20:59 -0300

    IF, ELIF e ELSE

---
+++ Python_Básico/condicoes.py
@@ -0,0 +1,14 @@
+# IF, ELIF e ELSE
+
+# if False:
+#     print("Expressão Verdadeira")
+# elif True:
+#     print("Agora a expressão é Verdadeira")
+# else:
+#     print("Expressão falsa")
+
+numero = input('Informe um numero: ')
+if int(numero) % 2 == 0:
+    print("Este numero é par")
+else:
+    print("Este numero é impar")
