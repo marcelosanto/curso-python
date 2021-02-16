ab04a5da3062ce7a24b496902246345912073b5d
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-15 21:03:00 -0300

    while/else

---
+++ Python_Básico/while_else.py
@@ -0,0 +1,12 @@
+# while e else
+
+contador = 1
+acumulador = 1
+
+while contador <= 25:
+    print(contador, acumulador)
+
+    acumulador += contador
+    contador += 1
+else:
+    print("Acabou")
