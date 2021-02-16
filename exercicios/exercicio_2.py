9b3cdf52721b82a13d8cc1ad38ffa752aee61078
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-16 09:49:52 -0300

    exercicio 2 feito

---
+++ exercicios/exercicio_2.py
@@ -0,0 +1,10 @@
+# x = 0
+# while x < 9:
+#     y = 10
+#     while y > 1:
+#         print(f'{x}, {y}')
+#         y -= 1
+#         x += 1
+
+for i, r in enumerate(range(10, 1, -1)):
+    print(i, r)
